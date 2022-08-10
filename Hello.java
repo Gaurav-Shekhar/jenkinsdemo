50211c7ff02696324f866b7c0a3d706efaeef34d
Gaurav-Shekhar <111014511+Gaurav-Shekhar@users.noreply.github.com>
2022-08-11 01:13:55 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+      public static void main(String a[])
+      {
+      System.out.Println("This file was modified");
+      for(i=0;i<10;i++)
+      {
+      System.out.Println("Hello world");
+      }
+      }
+}
